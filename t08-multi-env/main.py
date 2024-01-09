c7d8e6e214dbdf0a3822d049055ec685694397d9
Aditya Cahyo (work) <57148744+aditydcp@users.noreply.github.com>
2024-01-09 13:50:47 +0700

    Update main.py

--- t08-multi-env/main.py
+++ t08-multi-env/main.py
@@ -1,13 +1,13 @@
 import dotenv
 import subprocess
 
+# TODO: Implement GUI
+
 config = dotenv.dotenv_values(".env")
 
+# TODO: Change to GUI Input
 env1 = config.get('ENV_01_NAME')
 env2 = config.get('ENV_02_NAME')
-
-# print(env1)
-# print(env2)
 
 subprocess.run('conda run -n ' + env1 + ' python script.py', shell=True)
 subprocess.run('conda run -n ' + env2 + ' python script.py', shell=True)
